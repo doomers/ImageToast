d3076f66b89aa4a45578231be3e8868b6bc5cd0a
rahul357 <rahul40800@gmail.com>
2018-01-27 00:59:25 +0530

    demo prepared

--- app/src/main/java/com/doomers/phototoast/MainActivity.java
+++ app/src/main/java/com/doomers/phototoast/MainActivity.java
@@ -1,7 +1,11 @@
 package com.doomers.phototoast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.doomers.imagetoast.ImageToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.goku);
+
+        ImageToast.getInstance().show(this,bitmap);
     }
 }
---
+++ imagetoast/src/main/java/com/doomers/imagetoast/ImageToast.java
@@ -0,0 +1,31 @@
+package com.doomers.imagetoast;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class ImageToast {
+    private static ImageToast ourInstance = new ImageToast();
+
+    public static ImageToast getInstance() {
+        return ourInstance;
+    }
+
+    private ImageToast() {
+
+    }
+
+    public Toast show(Context context, Bitmap bitmap) {
+
+        Toast toast = new Toast(context);
+        ImageView view = new ImageView(context);
+        view.setImageBitmap(bitmap);
+        toast.setView(view);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.HORIZONTAL_GRAVITY_MASK,0,0);
+        toast.show();
+        return toast;
+    }
+}
